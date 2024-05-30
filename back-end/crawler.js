ef50e39d23c84967aeff9c8817fe3b18c5874c4d
thanhvinh-htnbt <thanhvinh.htn2020@gmail.com>
2024-05-30 17:44:40 +0700

    code cứng cào

--- back-end/crawler.js
+++ back-end/crawler.js
@@ -1,34 +1,143 @@
-const axios = require('axios');
+const puppeteer = require('puppeteer');
 const cheerio = require('cheerio');
 const fs = require('fs');
 
-async function crawlData() {
+async function crawlHotNovel(baseUrl) {
+
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    try {
+      await page.goto(baseUrl, { waitUntil: 'networkidle0' });
+
+      const htmlContent = await page.content();
+
+      const $ = cheerio.load(htmlContent);
+
+      const data = [];
+
+      $('.index-intro .item').each((index, element) => {
+        const title = $(element).find('.title h3').text();
+        const url = $(element).find('a').attr('href');
+        const imageUrl = $(element).find('img').attr('src');
+        data.push({ title, url, imageUrl });
+      });
+
+
+
+      fs.writeFileSync('hotnovels.json', JSON.stringify(data, null, 2));
+
+    } catch (error) {
+      console.error('Error:', error);
+    } finally {
+      await browser.close();
+    }
+}
+
+
+async function crawlNewNovel(baseUrl) {
+
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
   try {
+    await page.goto(baseUrl, { waitUntil: 'networkidle0' });
 
-    const response = await axios.get('https://truyenfull.vn/');
+    const htmlContent = await page.content();
 
-    const $ = cheerio.load(response.data);
+    const $ = cheerio.load(htmlContent);
 
-    //Cào các truyện mới cập nhật
     const data = [];
-    $('table tr').each((index, element) => {
-      const row = {
 
-        name: $(element).find('td:nth-child(1)').text(),
-        type: $(element).find('td:nth-child(2)').text(),
-        chapter: $(element).find('td:nth-child(3)').text(),
+    $('.list-truyen .row').each((index, element) => {
+      const title = $(element).find('.col-title h3 a').text().trim();
+      const url = $(element).find('.col-title h3 a').attr('href');
+      const genres = [];
+      $(element).find('.col-cat a').each((i, el) => {
+        genres.push($(el).text());
+      });
+      const chapter = $(element).find('.col-chap a').text().trim();
+      const time = $(element).find('.col-time').text().trim();
 
-      };
-
-
-      data.push(row);
+      data.push({ title, url, genres, chapter, time });
     });
 
+    fs.writeFileSync('newnovels.json', JSON.stringify(data, null, 2));
 
-    fs.writeFileSync('data/database.json', JSON.stringify(data, null, 2));
-    console.log('Dữ liệu đã được lưu vào tệp database.json');
   } catch (error) {
-    console.error('Lỗi khi thu thập dữ liệu:', error);
+    console.error('Error:', error);
+  } finally {
+    await browser.close();
   }
 }
-crawlData();
+
+async function crawlNovelType(baseUrl) {
+
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  try {
+    await page.goto(baseUrl, { waitUntil: 'networkidle0' });
+
+    const htmlContent = await page.content();
+
+    const $ = cheerio.load(htmlContent);
+
+    const data = [];
+
+    $('.list-truyen .row .col-xs-6 a').each((index, element) => {
+        const title = $(element).text().trim();
+        const url = $(element).attr('href');
+        data.push({ title, url });
+    });
+
+    fs.writeFileSync('noveltypes.json', JSON.stringify(data, null, 2));
+
+  } catch (error) {
+    console.error('Error:', error);
+  } finally {
+    await browser.close();
+  }
+}
+
+async function crawlChapter(baseUrl, storySlug, chapterSlug) {
+
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  try {
+    const url = `${baseUrl}/${storySlug}/${chapterSlug}/`;
+    await page.goto(url, { waitUntil: 'networkidle0' });
+
+    const htmlContent = await page.content();
+
+    const $ = cheerio.load(htmlContent);
+
+    const data = [];
+
+
+    const storyTitle = $('.truyen-title').text().trim();
+
+    const chapterTitle = $('.chapter-title').text().trim();
+
+    const chapterContent = $('#chapter-c').contents().filter(function() {
+      return this.nodeType === 3; // Chỉ lấy các nút văn bản
+    }).text().trim();
+
+    data.push({ storyTitle, chapterTitle, chapterContent });
+
+    fs.writeFileSync('chapter.json', JSON.stringify(data, null, 2));
+
+  } catch (error) {
+    console.error('Error:', error);
+  } finally {
+    await browser.close();
+  }
+}
+
+
+
+crawlHotNovel('https://truyenfull.vn');
+crawlNewNovel('https://truyenfull.vn');
+crawlNovelType('https://truyenfull.vn');
+crawlChapter('https://truyenfull.vn', 'ngao-the-dan-than', 'chuong-1');
